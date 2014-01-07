7cf43e52f17c709d2c4035537b2f2bbb16de5143
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2014-01-07 17:13:40 +0000

    Treat times as UTC
    This is not actually accurate but it makes live a lot simpler down the
    line.
    
    See https://www.pivotaltracker.com/story/show/62779118 for details.
    
    This commit also changes the `data_id` function to not include the
    dimension values as part of the id. This is because they are not passed
    through to backdrop and therefore can't be used to rebuild the id at a
    later stage if needed. Also, records are never identified by their
    dimension values within a single bucked (they would be in separate
    buckets).

--- collector/datetimeutil.py
+++ collector/datetimeutil.py
@@ -2,13 +2,11 @@ from datetime import datetime, time, timedelta, date
 import pytz
 
 
-GB_TIMEZONE = pytz.timezone("Europe/London")
-
 MONDAY = 0
 
 
 def to_datetime(a_date):
-    return GB_TIMEZONE.localize(datetime.combine(a_date, time(0)))
+    return datetime.combine(a_date, time(0)).replace(tzinfo=pytz.UTC)
 
 
 def to_utc(a_datetime):
--- collector/ga.py
+++ collector/ga.py
@@ -69,10 +69,9 @@ def _format(timestamp):
     return to_utc(timestamp).strftime("%Y%m%d%H%M%S")
 
 
-def data_id(data_type, timestamp, period, dimension_values):
+def data_id(data_type, timestamp, period):
     return base64.urlsafe_b64encode("_".join(
-        [data_type, _format(timestamp), period] + dimension_values
-    ))
+        [data_type, _format(timestamp), period]))
 
 
 def map_one_to_one_fields(mapping, pairs):
@@ -115,9 +114,7 @@ def build_document(item, data_type, start_date, mappings=None):
     period = "week"
     base_properties = {
         "_id": data_id(
-            data_type, to_datetime(start_date), period,
-            item.get("dimensions", {}).values()
-        ),
+            data_type, to_datetime(start_date), period),
         "_timestamp": to_datetime(start_date),
         "timeSpan": period,
         "dataType": data_type
