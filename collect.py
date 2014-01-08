041f9bfd7b8e5e64bb9ad117a7f3a11c6af5dff1
Tom Booth <tombooth@gmail.com>
2014-01-08 11:29:27 +0000

    Break up the line so that pep-8 passes

--- collect.py
+++ collect.py
@@ -6,7 +6,8 @@ from backdrop.collector.logging_setup import set_up_logging
 from collector.ga import send_records_for
 
 if __name__ == '__main__':
-    logfile_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'log')
+    logfile_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), 'log')
     set_up_logging('ga_collector', logging.DEBUG, logfile_path)
 
     args = arguments.parse_args('Google Analytics')
