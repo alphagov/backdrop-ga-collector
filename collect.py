2ee8200c3df4f0335a7d64b248ada45a69d30432
Nick Gravgaard <nick@beautifulcode.net>
2014-01-08 10:23:08 +0000

    Switching to latest version of backdrop-collector
    The latest version of backdrop-collector has an extra parameter in set_up_logging() which allows one to specify the logfile_path. Previously logfiles were being created in a place relative to cron's working directory.

--- collect.py
+++ collect.py
@@ -1,11 +1,13 @@
 import logging
+import os
 
 from backdrop.collector import arguments
 from backdrop.collector.logging_setup import set_up_logging
 from collector.ga import send_records_for
 
 if __name__ == '__main__':
-    set_up_logging('ga_collector', logging.DEBUG)
+    logfile_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'log')
+    set_up_logging('ga_collector', logging.DEBUG, logfile_path)
 
     args = arguments.parse_args('Google Analytics')
 
