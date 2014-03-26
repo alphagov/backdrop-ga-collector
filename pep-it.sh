a5c02e296451acd87f4aaf01674ce232a027362b
Peter Waller <peter@scraperwiki.com>
2014-03-26 07:58:38 +0000

    Ignore src and venv directories in pep8 check

--- pep-it.sh
+++ pep-it.sh
@@ -3,5 +3,5 @@
 set -e
 
 projectdir=$(dirname $0)
-pep8 --ignore=E201,E202,E241,E251 --exclude=tests "$projectdir"
+pep8 --ignore=E201,E202,E241,E251 --exclude=tests,src,venv "$projectdir"
 pep8 --ignore=E201,E202,E241,E251,E501 "$projectdir/tests"
