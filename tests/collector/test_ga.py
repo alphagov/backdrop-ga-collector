521597d1d2dca32cefd56014ee3e719e12e158fb
Peter Waller <peter@scraperwiki.com>
2014-03-26 07:59:18 +0000

    Tidy code and avoid unnecessary intermediate lists

--- tests/collector/test_ga.py
+++ tests/collector/test_ga.py
@@ -5,7 +5,9 @@ from hamcrest import assert_that, is_, has_entries, has_item, equal_to
 import mock
 from nose.tools import *
 from nose.tools import assert_is_instance
-from collector.ga import query_ga, build_document, data_id, apply_key_mapping, build_document_set, query_for_range, map_multi_value_fields
+from collector.ga import (query_ga, build_document, data_id, apply_key_mapping,
+                          build_document_set, query_for_range,
+                          map_multi_value_fields)
 from tests.collector import dt
 
 
