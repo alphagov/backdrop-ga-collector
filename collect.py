d9c2cecf56a3be9d91547c36fb6db8b6b8647dcb
Paul M Furley <paul@paulfurley.com>
2014-05-20 13:33:41 +0100

    Log the command line the collector is run with
    https://www.pivotaltracker.com/story/show/70748012
    
    Note that this depends on a new feature introduced in
    backdrop-collector-2.1.1
    
    [#70748012]

--- collect.py
+++ collect.py
@@ -1,14 +1,23 @@
 import logging
 import os
+import sys
 
 from backdrop.collector import arguments
 from backdrop.collector.logging_setup import set_up_logging
 from collector.ga import send_records_for
 
+
+def _extra_fields(argv):
+    return {
+        'command': ' '.join(argv)
+    }
+
+
 if __name__ == '__main__':
     logfile_path = os.path.join(
         os.path.dirname(os.path.realpath(__file__)), 'log')
-    set_up_logging('ga_collector', logging.INFO, logfile_path)
+    set_up_logging('ga_collector', logging.INFO, logfile_path,
+                   _extra_fields(sys.argv))
 
     args = arguments.parse_args('Google Analytics')
 
